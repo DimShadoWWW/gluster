b5a645a104e40c1c00e66c6ea31274e5511e1e72
Tom Bull <tom@tombull.com>
2016-03-31 05:52:59 +0100

    With a (hopefully working) entrypoint

--- host-noconfd/docker-entrypoint.sh
+++ host-noconfd/docker-entrypoint.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- glusterd "$@"
+fi
+
+if [ "$1" = 'glusterd' ]; then
+	shift
+	set -- glusterd -p /run/glusterd.pid "$@"
+  echo "Starting glusterd with command '$@'"
+  exec "$@"
+else
+  echo "Executing '$@'"
+  exec "$@"
+fi
